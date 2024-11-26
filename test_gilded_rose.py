9929abd5230b04049aa3ad923146495602b2d2f7
Louis Marx <louis.marx@mailo.com>
2024-11-26 23:16:13 +0100

    switch to pytest

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -1,15 +1,10 @@
-import unittest
+import pytest
 
 from gilded_rose import GildedRose, Item
 
 
-class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-        self.assertEqual("fixme", items[0].name)
-
-
-if __name__ == "__main__":
-    unittest.main()
+def test_foo():
+    items = [Item("foo", 0, 0)]
+    gilded_rose = GildedRose(items)
+    gilded_rose.update_quality()
+    assert items[0].name == "fixme"
